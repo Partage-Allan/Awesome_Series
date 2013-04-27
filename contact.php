0080853c460fbb24d9f620550dbf207893b40fc7
AzaFixe <AzaFixe@Azaelya>
2013-04-27 20:31:13 +0200

    modif et correction contact

--- contact.php
+++ contact.php
@@ -1,22 +1,69 @@
 <?php
-if(isset($_POST) && isset($_POST["nom"]) && isset($_POST['email']) && isset($_POST['message']))
-{
+if(!empty($_POST)){
     extract($_POST);
-    if(!empty($nom) && !empty($email) && !empty($message))
-    {
-        $message=str_replace("\'","'",$message);
-        $destinataire="Monkey_D_Alkan@gmx.fr";
-        $sujet="Formulaire de contact";
-        $msg="Une nouvelle question est arrivée \n
-        Nom: $nom \n
-        Email : $email \n
-        Message : $message";
-        $entete="From: $nom \n Reply-To: $email";
-        mail($destinataire, $sujet, $message, $entete);
+    $to = "Monkey_D_Alkan@gmx.fr";
+    $sujet = "Nouveau message de " . $nom;
+    $entete = "Email de: " . $nom . "\n";
+    $entete .= "Ayant pour adresse mail : " . $email . "\n";
+    
+    $entete .= "Voici son message: \n";
+    if(mail($to, $sujet, $commentaires, $entete)){
+        $erreur = "Votre message a bien ete envoye.";
+        unset($nom);
+        unset($email);
+        unset($commentaires);
     }
-    else
-    {
-        echo "Vous avez oublié de remplir un champ !";
+    else{
+        $erreur = "Une erreur est survenue, veuillez reessayer plus tard";
     }
 }
 ?>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<!-- FEULLES DE STYLES -->
+<link rel="stylesheet" type="text/css" href="css/awesome_series.css"/>
+<?php 
+    if (isset($erreur)){
+        if($erreur == "Votre message a bien ete envoye."){
+            echo '<script type="text/javascript">alert("Merci, votre message a bien ete envoye.")</script>';
+        }
+	else{
+            echo '<script type="text/javascript">alert("Une erreur est survenue, veuillez reessayer plus tard.")</script>';
+        }
+    }
+?>
+</head>
+
+<body>
+    <div id="ContainerForm">
+        <form method="post" action="contact.php" id="formulaire_contact">
+                <table>
+                <tr>    
+                    <td>
+                        <label for="nom">Nom : </label>
+                        <input type="text" id="nom" name="nom" required placeholder="Entrez votre nom" />
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label for="email">E-mail : </label>
+                        <input type="email" id="email" name="email" required placeholder="Entrez votre Email"/>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label for="commentaires">Commentaires :</label>
+                        <textarea id="commentaires" name="commentaires"  required placeholder="Entrez votre commentaire"></textarea>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <input type="submit" value="Envoyer le Formulaire" id="mySubmit" class="boutton"/>
+                    </td>
+                </tr>
+           </table> 
+        </form>
+    </div>
+</body>
+</html>

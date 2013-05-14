848db0487b696fb92204b0a6262534e01bbd3347
AzaFixe <AzaFixe@Azaelya>
2013-05-14 18:19:28 +0200

    - séparation de la connection BDD. - transfert de variables via URL pour les séries. - récupération de la variable pour affichage des infos: ----> requete BDD sur la série voulu. ----> requete BDD des acteurs correspondants à la série. ----> stockage des valeurs puis affichage: -----------> genre de la série dans le fil d'ariane.	| -----------> nom de la série dans le fil d'ariane.		| -----------> Affiche de la série + nom de la série.	|	Le tout, en fonction de la série + du nombre d'acteurs trouvé automatiquement. -----------> Synopsis de la série.				| -----------> Casting images + noms.			| -----------> Trailer de la série.				|
    TODO:
    Corriger l'encodage des caractère pour les ????? à la place des accents.
    J'avais la flemme de chercher et j'avais trop la tête dans le php :-p lol

--- fiche_serie.php
+++ fiche_serie.php
@@ -3,71 +3,84 @@
     <head>
         <meta charset="UTF-8"/>
         <link rel="stylesheet" href="./css/awesome_series.css"/>
-         <?php require ('template.php');?>
+         <?php 
+            require ('template.php');
+            require ('commun/sql.inc.php');
+         ?>
         <title>Aw3s0me Séries</title>
     </head>
     <body>
-        <?php afficher_header();?>
+        <?php 
+            afficher_header();
+            
+            $serie_select = str_replace('-', ' ', $_GET['serie']);
+            
+            $requete = 'SELECT * FROM serie
+                        WHERE titre_serie = \'' . $serie_select . '\' ';
+            $reponse = executer_requete($requete);
+            
+            while ($donnees = $reponse->fetch())
+            { 
+                $id_serie = $donnees['id_serie'];
+                $nom_serie = str_replace(' ', '-',$donnees['titre_serie']);
+                $serie = $donnees['titre_serie'];
+                $nationatile = $donnees['nationalite_serie'];
+                $realisateur = $donnees['realisateur_serie'];
+                $synopsis = $donnees['synopsis_serie'];
+                $resume = $donnees['resume_serie'];
+                $genre = $donnees['genre_serie'];
+                $annee = $donnees['annee_serie'];
+                $nb_saison = $donnees['nbre_saisons_serie'];
+                $statut = $donnees['statut_serie'];
+                $format = $donnees['format_serie'];
+                $trailer = $donnees['trailer_serie'];
+            }
+            
+            $requete2 = 'SELECT nom_acteur, role_acteur FROM casting_serie
+                             WHERE serie_id_serie = \'' . $id_serie . '\' ';
+            $reponse2 = executer_requete($requete2);
+            
+            $i = 0;
+            while ($donnees2 = $reponse2->fetch())
+            {
+                $casting[$i] = $donnees2['nom_acteur'];
+                $casting_image[$i] = str_replace(' ', '-',$donnees2['nom_acteur']);
+                $role[$i] = $donnees2['role_acteur'];
+                ++$i;
+            }
+
+            $reponse2->closeCursor();
+            $reponse->closeCursor();
+        ?>
         <section>
-            <p id="fil_d_ariane"><a href="index.php">Accueil</a> > <a href="">Series</a> > <a href="">Drama</a> > <a href="">Breaking Bad</a></p>
+            <p id="fil_d_ariane"><a href="index.php">Accueil</a> > <a href="">Series</a> > <a href=""><?php echo $genre; ?></a> > <a href=""><?php echo $serie; ?></a></p>
             <article>
-                <h1 id="affiche">Breaking Bad 
-                    <img src="images/affiche-breaking-bad.jpg" alt="affiche-breaking-bad"/>
+                <h1 id="affiche"><?php echo $serie; ?> 
+                    <img src="images/<?php echo $nom_serie; ?>.jpg" alt="affiche-<?php echo $nom_serie; ?>"/>
                 </h1>
-                <p id="synopsis">Walter White, 50 ans, est professeur de chimie dans un lycée du Nouveau-Mexique. Pour subvenir aux besoins de Skyler,
-                    sa femme enceinte, et de Walt Junior, son fils handicapé, il est obligé de travailler doublement. Son quotidien déjà morose devient carrément noir
-                    lorsqu'il apprend qu'il est atteint d'un incurable cancer des poumons. Les médecins ne lui donnent pas plus de deux ans à vivre. 
-                    Pour réunir rapidement beaucoup d'argent afin de mettre sa famille à l'abri, Walter ne voit plus qu'une solution : mettre ses connaissances en chimie
-                    à profit pour fabriquer et vendre du crystal meth, une drogue de synthèse qui rapporte beaucoup. Il propose à Jesse, un de ses anciens élèves devenu un petit dealer
-                    de seconde zone, de faire équipe avec lui. Le duo improvisé met en place un labo itinérant dans un vieux camping-car. Cette association inattendue va les entraîner dans 
-                    une série de péripéties tant comiques que pathétiques. 
+                <p id="synopsis"><?php echo $synopsis; ?>
                 </p>
                 
                 <div class="casting">
                     <p id="cast">Casting: </p>
                     <div class="liste_acteur">
                         <ul>
-                             <li>
-                                <img src="images/Bryan-Cranston-2.jpg" alt="Bryan Cranston" />
-                                <p>Bryan Cranston</p>
-                            </li>
-                            <li>
-                                <img src="images/Aaron-Paul.jpg" alt="Aaron Paul" />
-                                <p>Aaron Paul</p>
-                            </li>
-                            <li>
-                                <img src="images/Anna-Gunn.jpg" alt="Anna Gunn" />
-                                <p>Anna Gunn</p>
-                            </li>
-                                <li>
-                                <img src="images/Giancarlo-Esposito.jpg" alt="Giancarlo Esposito" />
-                                <p>Giancarlo Esposito</p>
-                            </li>
-                            <li>
-                                <img src="images/Dean-Norris.jpg" alt="Dean Norris" />
-                                <p>Dean Norris</p>
-                            </li>
-                            <li>
-                                <img src="images/Betsy-Brandt.jpg" alt="Betsy Brandt" />
-                                <p>Betsy Brandt</p>
-                            </li>
-                            <li>
-                                <img src="images/RJ-Mitte.jpg" alt="RJ Mitte" />
-                                <p>RJ Mitte</p>
-                            </li>
-                            <li>
-                                <img src="images/Jonathan-Banks.jpg" alt="Jonathan Banks" />
-                                <p>Jonathan Banks</p>
-                            </li>
-                            <li>
-                                <img src="images/Bob-Odenkirk-3.jpg" alt="Bob Odenkirk" />
-                                <p>Bob Odenkirk</p>
-                            </li>
+                            <?php
+                                $j = 0;
+                                while($j != count($role))
+                                {
+                                    printf('<li>');
+                                    printf('<img src="images/' . $casting_image[$j] . '.jpg" alt="' . $casting[$j] . '" />');
+                                    printf('<p>' . $casting[$j] . '</p>');
+                                    ++$j;
+                                }
+                            ?>
+                        </ul>
                     </div>
                 </div>
                 <div class="bloc_trailer">
                     <p class="texte_trailer">Trailer :</p><br/>
-                <iframe width="560" height="315" src="http://www.youtube.com/embed/kXPfG8C4wpA" frameborder="0" allowfullscreen></iframe>
+                <iframe width="560" height="315" src="<?php echo $trailer; ?>" frameborder="0" allowfullscreen></iframe>
                 </div>
                 
                    
--- search.php
+++ search.php
@@ -3,7 +3,10 @@
     <head>
         <meta charset="UTF-8"/>
         <link rel="stylesheet" href="./css/awesome_series.css"/>
-         <?php require ('template.php');?>
+         <?php 
+            require ('template.php');
+            require ('commun/sql.inc.php');
+         ?>
         <title>Aw3s0me Séries</title>
     </head>
     <body>
@@ -11,26 +14,18 @@
         <section>
             <p id="fil_d_ariane"><a href="index.php">Accueil</a> > <a href="search.php">Series</a></p>
             <?php
-                try
-                {
-                    $bdd = new PDO('mysql:host=localhost;dbname=awesome_series', 'root', '');
-                }
-                catch (Exception $e)
-                {
-                        die('Erreur : ' . $e->getMessage());
-                }
-                
                 if(isset($_POST['search']))
                 {
-                    $search= $_POST['search'];
-                    $reponse = $bdd->query('SELECT titre_serie 
-                                            FROM serie
-                                            WHERE titre_serie LIKE \'' .$search . '%\' ');
+                    $search = $_POST['search'];
+                    $requete_precise = 'SELECT titre_serie FROM serie
+                                        WHERE titre_serie LIKE \'' .$search . '%\' ';
+                    
+                    $reponse = executer_requete($requete_precise);
                 }
                 else
                 {
-                    $reponse = $bdd->query('SELECT titre_serie 
-                                            FROM serie');
+                    $requete_general = 'SELECT titre_serie FROM serie';
+                    $reponse = executer_requete($requete_general);
                 }
                 
             ?>
@@ -41,7 +36,7 @@
                     while ($donnees = $reponse->fetch())
                     { 
                         $nom_serie = str_replace(' ', '-',$donnees['titre_serie']);
-                        echo '<a class="search" href="fiche_serie.php?' . $nom_serie . '">' . '- ' . $donnees['titre_serie'] . '</a>' . '<br /><br />';
+                        echo '<a class="search" href="fiche_serie.php?serie=' . $nom_serie . '">' . '- ' . $donnees['titre_serie'] . '</a>' . '<br /><br />';
                     }
 
                     $reponse->closeCursor();

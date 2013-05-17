1ab59218ceb2267dbd589cd8f8e40dcd4d66c602
hugo <hugo@192.168.1.6>
2013-05-17 22:22:45 +0200

    Revert ""
    This reverts commit 6f5207869da097cc5c0f6168c8a7574290277a90.

--- commun/sql.inc.php
+++ commun/sql.inc.php
@@ -3,7 +3,7 @@ function executer_requete ($requete)
 {
     try
     {
-        $bdd = new PDO('mysql:host=localhost;dbname=awesome_series', 'root', '', array(PDO::MYSQL_ATTR_INIT_COMMAND =>  'SET NAMES utf8') );
+        $bdd = new PDO('mysql:host=localhost;dbname=awesome_series', 'root', '');
     }
     catch (Exception $e)
     {
